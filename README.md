54f3e100232ab84179196739e2c6d80dccdace39
nikniv <niveditn@gmail.com>
2021-01-01 11:52:12 +0530

    commit eml source

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# embeddable-media-library
+# Embeddable Media Library
+
+# Usage
+
+Serve the `sample-app` as follows:
+
+```
+cd samples/sample-app
+npm i
+npm start
+```
+
+Or, simply open `samples/sample-app/index.html` in any browser. (Firefox recommended.)
+
+For HTTPS mode on localhost, see further below.
+
+## Configure IK_HOST
+
+The constant `IK_HOST` defines the dashboard instance used as the iframe source in the plugin. Its default value is `https://stage-eml.imagekit.io`.
+This can be changed in the first few lines of `src/imagekit-eml.js`.
+
+## Simple HTTPS on localhost
+
+For the sample-app to be served over `https`, create a simple SSL certificate using `mkcert` tool.
+
+Installation:
+
+```
+brew install mkcert
+```
+
+Generate the certificate:
+
+```
+cd samples/sample-app/
+mkcert -install
+mkcert localhost
+```
+
+Restart Firefox browser in order for the certificate to take effect.
+
+Now, serve the sample-app securely:
+
+```
+npm run start:secure
+```
+
+The app should be available on `https://localhost:8080`. 
---
+++ src/index.js
@@ -0,0 +1,143 @@
+import styles from './styles.css'
+
+export default function ImageKitEML() {
+  // const IK_HOST = 'https://imagekit.io';
+  // const IK_HOST = 'https://stage.imagekit.io';
+  const IK_HOST = 'https://stage-eml.imagekit.io';
+  // const IK_HOST = 'https://dev4.imagekit.io';
+  const IK_SRC = `${IK_HOST}/dashboard#inline-media-library`;
+
+  // Define constructor 
+  this.IKFrame = function () {
+    // Create global element references
+    this.security = null;
+
+    // Define option defaults 
+    var defaults = {
+      className: "ik-frame",
+      container: "",
+      containerDimensions: {
+        height: '100%',
+        width: '100%'
+      },
+      dimensions: {
+        height: '100%',
+        width: '100%'
+      },
+      name: 'ImageKit Media Library',
+      style: {
+        border: 'none'
+      },
+      view: 'modal',
+      showOpenButton: true,
+    };
+
+    // Create options by extending defaults with the passed in arugments
+    if (arguments[0] && typeof arguments[0] === "object") {
+      this.options = Object.assign({}, defaults, arguments[0]);
+    }
+
+    // Set callback function
+    if (arguments[1] && typeof arguments[1] === "function") {
+      this.callback = arguments[1];
+    }
+
+    this.init();
+  }
+
+  IKFrame.prototype.init = function () {
+    buildOut.call(this);
+  }
+
+  var button, modal, modalContent; // modal-only elements
+
+  function buildOut() {
+    var container, docFragment, mainFrame; // common elements
+
+    // If container is an HTML string, find the DOM node, else use it directly.
+    if (typeof this.options.container === "string") {
+      container = document.querySelector(this.options.container);
+    } else {
+      container = this.options.container;
+    }
+
+    // Create a DocumentFragment to build with
+    docFragment = document.createDocumentFragment();
+
+    // Create ikFrame element
+    this.ikFrame = document.createElement("div");
+    this.ikFrame.className = this.options.className;
+    this.ikFrame.style.height = this.options.containerDimensions.height;
+    this.ikFrame.style.width = this.options.containerDimensions.width;
+    this.ikFrame.callback = this.callback;
+
+    mainFrame = document.createElement("iframe");
+    mainFrame.title = this.options.name;
+    mainFrame.src = IK_SRC;
+    mainFrame.sandbox = 'allow-top-navigation allow-same-origin allow-scripts allow-forms';
+    mainFrame.height = this.options.dimensions.height;
+    mainFrame.width = this.options.dimensions.width;
+    mainFrame.style.border = this.options.style.border;
+
+    this.ikFrame.appendChild(mainFrame);
+
+    if (this.options.view.toLowerCase() !== 'modal') {
+      // Append ikFrame to DocumentFragment
+      docFragment.appendChild(this.ikFrame);
+
+      // Append DocumentFragment to body
+      container.appendChild(docFragment);
+    } else {
+      if (this.options.showOpenButton) {
+        // create button
+        button = document.createElement("button");
+        button.innerHTML = "Open Media Library";
+        button.onclick = function () {
+          modal.style.display = "block";
+        }
+      }
+
+      // create modal
+      modal = document.createElement("div");
+      modalContent = document.createElement("div");
+      modal.classList.add("modal");
+      modal.classList.add("ik-eml-modal");
+      modalContent.classList.add("modal-content");
+      modalContent.appendChild(this.ikFrame);
+      modal.appendChild(modalContent);
+
+      // append button and modal to docFragment
+      if (this.options.showOpenButton) {
+        docFragment.appendChild(button);
+      }
+      docFragment.appendChild(modal);
+
+      // append docFragment to container
+      container.appendChild(docFragment);
+    }
+  }
+
+  const closeModal = () => {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      closeModal();
+    }
+  }
+
+  // Called sometime after postMessage is called
+  window.addEventListener("message", (event) => {
+    if (event.origin !== IK_HOST) {
+      return;
+    }
+
+    if (event.data.eventType === 'CLOSE_EML') {
+      closeModal();
+    } else if (event.data.eventType === 'INSERT') {
+      this.callback(event.data);
+      closeModal();
+    }
+  });
+}
